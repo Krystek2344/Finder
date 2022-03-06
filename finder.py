9f8fe8e4644ccd16aa8ec2de5ff9208d9ffad73d
Krystek2344 <krystian.oprzala@gmail.com>
2022-03-06 22:49:59 +0100

    url root with initial search function

--- finder.py
+++ finder.py
@@ -2,12 +2,24 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-part = input("Podaj numer: ")
+# PAGE = 1
+# &page={PAGE}
 
+part = str(input("Podaj numer: "))
 url = f"https://www.iparts.pl/znajdz/?idCar=&query={part}"
+
 page = requests.get(url).text
 
 doc = BeautifulSoup(page, "html.parser")
 
-page_text = doc.find(class_="number-pager")
-print(page_text)
+div = doc.find(class_="katalog-lista row")
+item = div.find(text=re.compile(part))
+
+# items_l = items.split(' ')
+#
+# print(items_l)
+print(item)
+
+parent = item.find_parent(class_="produkt middle-12")
+
+
